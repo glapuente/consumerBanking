c4df56e248a477499481e5ba35130667e2aa8e9c
Guillermo Lapuente <glapuentevalea@gmail.com>
2016-09-14 12:57:47 +0200

    Updated files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,6 @@
 public class App {
     public static void main(String[] args) {
         Util.printMessage("Hello World!");
-        Util.printMssage("Kaixo Mundua!");
+        Util.printMessage("Kaixo Mundua!");
     }
 }
